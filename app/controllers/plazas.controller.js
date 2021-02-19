db775825f55d78842f87666db8133d7d50569699
javg15 <javg15@gmail.com>
2021-02-18 21:20:33 -0600

    Update plazas.controller.js

--- app/controllers/plazas.controller.js
+++ app/controllers/plazas.controller.js
@@ -226,13 +226,13 @@ exports.setRecord = async(req, res) => {
                 return value; // Sanitize: remove all special chars except numbers
             }
         },
-        id_catcentrostrabajo: {
+        /*id_catcentrostrabajo: {
             type: "number",
             custom(value, errors) {
                 if (value <= 0) errors.push({ type: "selection" })
                 return value; // Sanitize: remove all special chars except numbers
             }
-        },
+        },*/
         /*id_catplantelescobro: { type: "number" },
         id_catzonageografica: { type: "number" },*/
         id_catestatusplaza: {
