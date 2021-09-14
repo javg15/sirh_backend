663510e6046f981fe423e1dc0b2bf63d49a57eca
javg15 <javg15@gmail.com>
2021-09-13 20:17:55 -0500

    historial nomina

--- app/models/index.js
+++ app/models/index.js
@@ -18,10 +18,31 @@ const sequelize = new Sequelize(
     }
 );
 
+//para el historial de nomina
+const sequelizeNomina = new Sequelize(
+    config.DBNomina,
+    config.USER,
+    config.PASSWORD, {
+        host: config.HOST,
+        dialect: config.dialect,
+        operatorsAliases: config.operatorsAliases,
+        timezone: '-06:00'
+            /*pool: {
+                max: config.pool.max,
+                min: config.pool.min,
+                acquire: config.pool.acquire,
+                idle: config.pool.idle
+            }*/
+    }
+);
+
+
 const db = {};
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
+//historial de plazas segun sistema de nomina
+db.sequelizeNomina = sequelizeNomina;
 
 db.user = require("../models/user.model.js")(sequelize, Sequelize);
 db.usuarios_zonas = require("../models/usuarios_zonas.model.js")(sequelize, Sequelize);
