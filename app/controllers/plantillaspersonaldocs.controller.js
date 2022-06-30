129b3dad82676af19c3e18ce78ec28b9b435bb24
javg15 <javg15@gmail.com>
2022-06-29 23:16:29 -0500

    Update plantillaspersonaldocs.controller.js

--- app/controllers/plantillaspersonaldocs.controller.js
+++ app/controllers/plantillaspersonaldocs.controller.js
@@ -37,8 +37,8 @@ exports.getAdmin = async(req, res) => {
         query = "SELECT * FROM s_plantillaspersonaldocs_mgr('" +
             "&modo=:modo&id_usuario=:id_usuario" +
             "&inicio=:start&largo=:length" +
-            "&ordencampo=Inicio|Fin" +
-            "&ordensentido=DESC|DESC" +
+            "&ordencampo=Inicio|Fecha|Fin" +
+            "&ordensentido=DESC|DESC|DESC" +
             "&fkey=" + params.opcionesAdicionales.fkey +
             "&fkeyvalue=" + params.opcionesAdicionales.fkeyvalue.join(",") + "')";
 
